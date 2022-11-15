04a3ffd328000d4ca5c0a3166c1c7ff2f015cb91
Ethan NGHIEN <113614747+Stanewolfer@users.noreply.github.com>
2022-11-15 16:50:13 +0100

    Create doc.php

---
+++ doc.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/all.min.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+    <link rel = "stylesheet" href="style.css">
+
+
+    <!-- importation font family "Anton"-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+
+    <!-- ilmportation font family "Archivo"-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300&display=swap" rel="stylesheet">
+
+    <!-- importation icones-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"/>
+    
+    <title>Document</title>
+</head>
+
+<body>
+<body class = "body2">
+<div class = "fond">
+<header>
+    <nav>
+        <p class="power"><a href="doc.html" class="connexion"> The power of memory</a></p>
+        <p class = "pad"><a href="jeu.html" class="connexion"> JEU</a></p>
+        <p><a href="scores.html" class="connexion"> SCORES</a></p>
+        <p><a href="inscription.html" class = "connexion"> INSCRIPTION </a></p>
+        <p><a href="connexion.html"  class="connexion"> CONNEXION</a></p>
+        <p><a href="myaccount.html" class="connexion"> MON ESPACE </a></p>
+        <p class="pad1"><a href="contact.html" class="connexion">NOUS CONTACTER</a></p>
+    </nav>
+</header>
+
+<section>
+    <h1 class = "Bienvenue">BIENVENUE DANS <br> NOTRE STUDIO!</h1>
+    <p class = "Bienvenue1">Venez challenger les cerveaux les plus agiles!</p>
+
+<div class = button>
+    <button class="jouer">JOUER!</button>
+</div>
+</section>
+</div>
+
+<div class="container">
+    <section>
+        <div class = section2>
+            <img src = "Images/retrogaming-console.jpeg" alt = "img1" class = "img1">
+            <img src = "Images/trotinette.png" alt="img2" class="img2">
+            <img src = "Images/royal flush.png" alt="img3" class="img3">
+        </div>
+        <div class = "section5">
+            <div class = "un">
+                <h1><span class="number">01</span> LOREM IPSUM</h1>
+                <p class="deux">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur laboriosam ad dolor facilis architecto quasi corrupti earum illum quidem quod eligendi nulla impedit itaque eos sapiente excepturi, quaerat nemo vitae.</p>
+            </div>
+            <div class = "un">
+                <h1><span class="number">02</span> LOREM IPSUM</h1>
+                <p class="deux">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime totam iusto magni repellat possimus tempora, ipsum voluptatem alias amet reprehenderit doloribus, ad, accusantium repudiandae rerum? Dignissimos dolore recusandae natus optio!</p>
+            </div>
+            <div class = "un">
+                <h1><span class="number">03</span> LOREM IPSUM</h1>
+                <p class="deux">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime totam iusto magni repellat possimus tempora, ipsum voluptatem alias amet reprehenderit doloribus, ad, accusantium repudiandae rerum? Dignissimos dolore recusandae natus optio!</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class = "section3">
+    <img src = "Images/Background/img connexion.webp" alt = "img4" class="watchdog">
+     <div class = "conteneur">
+        <div class="I">310 <br>Parties Jouées</div>
+        <div class="J">1020 <br>Joueurs Connectés</div>
+        <div class="K">10 sec<br>Temps Record </div>
+        <div class="L">21300 <br>Joueurs Inscrits</div>
+    </div>
+</section>
+
+<div class="equipe">
+    <h1><strong>Notre équipe</strong></h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quam?</p>
+     <img src="Images/Logo/fleur-de-lys.jpeg" alt = "img5" id="fleur">
+</div>
+
+<section class="section4">
+    <div class="card1">
+        <img src="Images/Perso/perso1.webp" id="per1">
+        <h2>HAMILTON</h2>
+        <p>Game Designer</p>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-pinterest"></i>
+        <i class="fa-brands fa-instagram"></i>
+    </div>
+    
+    <div class ="card2">
+        <img src="Images/Perso/perso2.webp" id="per2">
+        <h2>MICKAEL</h2>
+        <p>Game Designer</p>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-pinterest"></i>
+    </div>
+    
+    <div class="card3">
+    <img src="Images/Perso/perso3.webp" id="per3">
+    <h2>ARNOLD</h2>
+    <p>Game Designer</p>
+    <i class="fa-brands fa-facebook-f"></i>
+    <i class="fa-brands fa-twitter"></i>
+    <i class="fa-brands fa-pinterest"></i>
+</div>       
+</section>
+
+<footer>
+    <div class="footer1">
+        <h1>Informations</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dolore!</p>
+        <p> <div class="orange"> Mail: </div> support@powerofmemory.com</p>
+        <p><div class="orange">Location : </div> Paris</p>
+                
+    <div class="icones">
+        <div class="icone1">
+            <i class="fa-brands fa-facebook"></i>
+        </div>
+        <div class="icone2">
+            <i class="fa-brands fa-twitter"></i>
+        </div>
+        <div class="icone3">
+            <i class="fa-brands fa-google"></i>
+        </div>
+        <div class="icone4">
+            <i class="fa-brands fa-pinterest"></i>
+        </div>
+        <div class="icone5">
+            <i class="fa-brands fa-instagram"></i>
+    </div>
+</div>
+    
+<p class="copyright">Copyright © 2022 Tous droits réservés</p>
+</div>
+<div class="footer2">
+    <h1>Power Of Memory</h1>
+    <ul>
+        <li>Jouer!</li>
+        <li>Les scores</li>
+        <li>Nous contacter</li>
+    </ul>
+</div>
+</footer>
+</body>
+</html>
