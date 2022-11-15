fc96e4e4ff533ed7aed36e560e0157fcd685e9cf
lucasTrswl <lucasturschwell@hotmail.com>
2022-11-15 15:37:51 +0100

    ok

---
+++ database/database.inc.php
@@ -0,0 +1,9 @@
+
+<?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = 'root';
+            
+            //On établit la connexion
+            $conn = new PDO("mysql:host=$servername;dbname=MySQL1", $username, $password);
+        ?>
