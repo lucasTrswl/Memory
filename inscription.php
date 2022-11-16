6ed5daee0534e7ace02691cc019a83347f00f1cb
Ethan NGHIEN <113614747+Stanewolfer@users.noreply.github.com>
2022-11-16 09:19:07 +0100

    modif

--- inscription.php
+++ inscription.php
@@ -33,13 +33,13 @@
 <body class = "body2">
 <header>
     <nav>
-        <p class="power"><a href="doc.html" class="connexion"> The power of memory</a></p>
-        <p class = "pad"><a href="jeu.html" class="connexion"> JEU</a></p>
-        <p><a href="scores.html" class="connexion"> SCORES</a></p>
-        <p><a href="inscription.html" class = "connexion"> INSCRIPTION </a></p>
-        <p><a href="connexion.html"  class="connexion"> CONNEXION</a></p>
-        <p><a href="myaccount.html" class="connexion"> MON ESPACE </a></p>
-        <p class="pad1"><a href="contact.html" class="connexion">NOUS CONTACTER</a></p>
+        <p class="power"><a href="doc.php" class="connexion"> The power of memory</a></p>
+        <p class = "pad"><a href="jeu.php" class="connexion"> JEU</a></p>
+        <p><a href="scores.php" class="connexion"> SCORES</a></p>
+        <p><a href="inscription.php" class = "connexion"> INSCRIPTION </a></p>
+        <p><a href="connexion.php"  class="connexion"> CONNEXION</a></p>
+        <p><a href="myaccount.php" class="connexion"> MON ESPACE </a></p>
+        <p class="pad1"><a href="contact.php" class="connexion">NOUS CONTACTER</a></p>
     </nav>
 </header>
 
