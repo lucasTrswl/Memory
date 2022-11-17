05d402fe4528798158d0c0629a15176eeb0f4eb6
Stanewolfer <seiya0500@gmail.com>
2022-11-17 15:24:41 +0100

    Update header.inc.php

--- view/header.inc.php
+++ view/header.inc.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <p class="power"><a href="doc.php" class="connexion"> The power of memory</a></p>
-        <p class="pad"><a href="heu.php" class="connexion">JEU</a></p>
+        <p class="pad"><a href="jeu.php" class="connexion">JEU</a></p>
         <p><a href="scores.php" class="connexion"> SCORES</a></p>
         <p><a href="inscription.php" class = "connexion"> INSCRIPTION </a></p>
         <p><a href="connexion.php"  class="connexion"> CONNEXION</a></p>
