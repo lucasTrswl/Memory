1459ae68cf6662264b5647c2b7e4158921354649
YimoSsDev <maeltordjmanpro@gmail.comm>
2022-11-25 09:04:07 +0100

    v

--- jeu.js
+++ jeu.js
@@ -125,3 +125,63 @@ function début(){
     }
 }
 bouton.addEventListener('click',début);
+for (var i = 0; i < bgCardsLength; i++) {
+    card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.innerHTML = "<figure class='back'></figure><figure class='front'></figure>";
+    plateau.appendChild(card);
+    //Calcul de la hauteur en fonction de la largeur - qui s'adapte en responsive
+    cardSize = $('.card').width();
+    $('.card').css('height', cardSize)
+    front = card.childNodes[1];
+    bg = "url(" + bgCards[i] + ") no-repeat";
+    front.style.background = bg;
+    card.addEventListener('click', function() {
+        addFlipped(this);
+    });
+} 
+function reset() {
+    cards = document.querySelectorAll(".card");
+    for (var u = 0; u < cards.length; u++) {
+        cards[u].classList.remove('flipped');
+    }
+    $("#menu").fadeOut("fast");
+    numberCaught = 0;
+    caughtField.innerHTML = numberCaught;
+    seconds = 0;
+}
+function shuffle(tab) {
+    for (i = tab.length; i > 0; i--) {
+        j = Math.floor(Math.random() * i);
+        x = tab[i - 1];
+        tab[i - 1] = tab[j];
+        tab[j] = x;
+    }
+}
+function addFlipped(target) {
+    if (!target.classList.contains('flipped')) {
+        target.classList.add('flipped');
+        flippedCards.push(target);
+        if (flippedCards.length > 1) {
+            removeFlipped();
+        }
+    }
+}
+function removeFlipped() {
+    if (flippedCards.length > 2) {
+        flippedCards[0].classList.remove('flipped');
+        flippedCards.shift();
+    }
+    checkResult();
+}
+function checkResult() {
+    if (flippedCards[0].innerHTML == flippedCards[1].innerHTML) {
+        numberCaught++;
+        caughtField.innerHTML = numberCaught;
+        flippedCards = [];
+        if (numberCaught == bgCardsLength / 2) {
+            $("#menu").fadeIn("fast");
+            timeField.innerHTML = seconds;
+        }
+    }
+}
